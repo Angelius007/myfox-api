188af77d42705281ca468aac2999f31ee82dad18
Angelius007 <github@angelius.bzh>
2024-09-08 11:59:41 +0200

    Modif enum light sensor

--- custom_components/myfox/const.py
+++ custom_components/myfox/const.py
@@ -3,11 +3,21 @@ DOMAIN_MYFOX = "myfox"
 CONFIG_VERSION = 1
 
 ONLINE_OPTIONS = {
-    "Offline": 1,
     "Online": 0,
+    "Offline": 1,
 }
 
 ALERTE_OPTIONS = {
-    "ALERTE": 1,
     "OK": 0,
+    "ALERTE": 1,
+}
+
+LIGHT_OPTIONS = {
+    "LVL0 (0)": 0,
+    "LVL1 (1)": 1,
+    "LVL2 (2)": 2,
+    "Lumière du jour (3)": 3,
+    "Lumière basse (4)": 4,
+    "Pénombre (5)": 5,
+    "Obscurité (6)": 6,
 }
--- custom_components/myfox/sensor.py
+++ custom_components/myfox/sensor.py
@@ -2,12 +2,12 @@ import logging
 
 from homeassistant.components.sensor import (SensorDeviceClass, SensorStateClass)
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import (UnitOfTemperature, LIGHT_LUX)
+from homeassistant.const import (UnitOfTemperature)
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import (DOMAIN_MYFOX,ALERTE_OPTIONS,ONLINE_OPTIONS)
+from .const import (DOMAIN_MYFOX, ALERTE_OPTIONS, ONLINE_OPTIONS, LIGHT_OPTIONS)
 from .api.myfoxapi import (MyFoxApiClient)
 from .coordinator.myfox_coordinator import (MyFoxCoordinator)
 from .entities import BaseSensorEntity,DictStateBaseSensorEntity
@@ -32,12 +32,11 @@ class TempSensorEntity(BaseSensorEntity):
     _attr_native_value = -1
 
 
-class LightSensorEntity(BaseSensorEntity):
-    _attr_device_class = SensorDeviceClass.ILLUMINANCE
+class LightSensorEntity(DictStateBaseSensorEntity):
+    _attr_device_class = SensorDeviceClass.ENUM
     _attr_entity_category = EntityCategory.DIAGNOSTIC
-    _attr_native_unit_of_measurement = LIGHT_LUX
     _attr_state_class = SensorStateClass.MEASUREMENT
-    _attr_native_value = -1
+    _options_dict: dict[str, int] = LIGHT_OPTIONS
 
 
 class OnlineSateSensorEntity(DictStateBaseSensorEntity):
