a72a3c7684af40473e4129cf911c726a398807d6
Angelius007 <github@angelius.bzh>
2024-09-11 21:05:13 +0200

    add reconfig

--- custom_components/myfox/config_flow.py
+++ custom_components/myfox/config_flow.py
@@ -28,35 +28,69 @@ from .devices.site import MyFoxSite
 class MyFoxConfigFlow(ConfigFlow, domain=DOMAIN_MYFOX):
     """ Config """
     VERSION = CONFIG_VERSION
+    PREFIX_ENTRY = "myfox-"
 
     # Init des variables locales
     def __init__(self) -> None:
         self.myfox_client:MyFoxApiClient = None
+        self.siteId = None
         self.site: MyFoxSite = None
         self.sites: list[MyFoxSite] = field(default_factory=list[MyFoxSite])
+
+        self.client_id = None
+        self.client_secret = None
+        self.username = None
+        self.password = None
+
+        self.access_token = None
+        self.refresh_token = None
 
         self.config_entry: ConfigEntry | None = None
 
     # Step pour relancer la conf
-    async def async_step_reconfigure_user(self, user_input: dict[str, Any] | None = None):
-        if not user_input:
-            return self.async_show_menu(
-                step_id="user"
-            )
+    async def async_step_reconfigure_user(self, user_input: dict[str, Any] | None = None): 
+        if "entry_id" in self.context and self.context["entry_id"] :
+            unique_id = self.context["entry_id"] #.replace(self.PREFIX_ENTRY, "")
+            existing_entry = await self.async_set_unique_id(unique_id, raise_on_progress=False)
+            if existing_entry:
+                data = existing_entry.data.copy()
+                if KEY_CLIENT_ID in data:
+                    self.client_id = data[KEY_CLIENT_ID]
+                if KEY_CLIENT_SECRET in data:
+                    self.client_secret = data[KEY_CLIENT_SECRET]
+                if KEY_MYFOX_USER in data:
+                    self.username = data[KEY_MYFOX_USER]
+                if KEY_MYFOX_PSWD in data:
+                    self.password = data[KEY_MYFOX_PSWD]
+                if KEY_ACCESS_TOKEN in data:
+                    self.access_token = data[KEY_ACCESS_TOKEN]
+                if KEY_REFRESH_TOKEN in data:
+                    self.refresh_token = data[KEY_REFRESH_TOKEN]
+                if KEY_SITE_ID in data:
+                    self.siteId = data[KEY_SITE_ID]
+
+        return await self.async_step_user()
 
     # 1er step
     async def async_step_user(self, info: dict[str, Any] | None = None):
         USER_STEP_SCHEMA = vol.Schema({
-            vol.Required(KEY_CLIENT_ID): str,
-            vol.Required(KEY_CLIENT_SECRET): str,
-            vol.Required(KEY_MYFOX_USER): str,
-            vol.Required(KEY_MYFOX_PSWD): str
+            vol.Required(KEY_CLIENT_ID, default=self.client_id): str,
+            vol.Required(KEY_CLIENT_SECRET, default=self.client_secret): str,
+            vol.Required(KEY_MYFOX_USER, default=self.username): str,
+            vol.Required(KEY_MYFOX_PSWD, default=self.password): str,
+            vol.Optional(KEY_ACCESS_TOKEN, default=self.access_token): str,
+            vol.Optional(KEY_REFRESH_TOKEN, default=self.refresh_token): str,
         })
         if info is not None:
             myfox_info = MyFoxEntryDataApi(info.get(KEY_CLIENT_ID),
                                         info.get(KEY_CLIENT_SECRET),
                                         info.get(KEY_MYFOX_USER),
                                         info.get(KEY_MYFOX_PSWD))
+            if KEY_ACCESS_TOKEN in info :
+                myfox_info.access_token = info.get(KEY_ACCESS_TOKEN)
+            if KEY_REFRESH_TOKEN in info :
+                myfox_info.refresh_token = info.get(KEY_REFRESH_TOKEN)
+            
             self.myfox_client = MyFoxApiClient(myfox_info)
             
             login_ok = await self.myfox_client.login()
@@ -77,7 +111,7 @@ class MyFoxConfigFlow(ConfigFlow, domain=DOMAIN_MYFOX):
             for site in self.sites:
                 if site.key == info.get(KEY_SITE_ID) :
                     self.site = site
-            device_unique_id = "myfox-"+str(self.site.siteId)
+            device_unique_id = self.PREFIX_ENTRY+str(self.site.siteId)
             
             existing_entry = await self.async_set_unique_id(device_unique_id)
             
@@ -116,7 +150,7 @@ class MyFoxConfigFlow(ConfigFlow, domain=DOMAIN_MYFOX):
             site_list.append(site.key)
 
         SITE_STEP_SCHEMA = vol.Schema({
-            vol.Required(KEY_SITE_ID): selector.SelectSelector(
+            vol.Required(KEY_SITE_ID, default=self.siteId): selector.SelectSelector(
                 selector.SelectSelectorConfig(options=site_list,
                                                 mode=selector.SelectSelectorMode.DROPDOWN))
         })
