07c538ea6da652704a81d482dae0b1237c295a8b
Angelius007 <github@angelius.bzh>
2024-11-22 10:24:59 +0100

    improved hass.data and schedule reload

--- custom_components/myfox/number.py
+++ custom_components/myfox/number.py
@@ -3,16 +3,18 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.util.hass_dict import HassEntryKey
 
 from .const import (DOMAIN_MYFOX)
 from .coordinator.myfox_coordinator import (MyFoxCoordinator)
 from .api.myfoxapi import (MyFoxApiClient)
 
 _LOGGER = logging.getLogger(__name__)
+MYFOX_KEY: HassEntryKey["MyFoxCoordinator"] = HassEntryKey(DOMAIN_MYFOX)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
     """ Chargement des switchs """
-    coordinator:MyFoxCoordinator = hass.data[DOMAIN_MYFOX][entry.entry_id]
+    coordinator:MyFoxCoordinator = hass.data.setdefault(MYFOX_KEY, {})[entry.entry_id]
     for (client_key,client_item) in coordinator.myfoxApiClients.items() :
         client: MyFoxApiClient = client_item
 
--- custom_components/myfox/sensor.py
+++ custom_components/myfox/sensor.py
@@ -3,16 +3,18 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.util.hass_dict import HassEntryKey
 
 from .const import (DOMAIN_MYFOX)
 from .api.myfoxapi import (MyFoxApiClient)
 from .coordinator.myfox_coordinator import (MyFoxCoordinator)
 
 _LOGGER = logging.getLogger(__name__)
+MYFOX_KEY: HassEntryKey["MyFoxCoordinator"] = HassEntryKey(DOMAIN_MYFOX)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
     """ Chargement des sensors """
-    coordinator:MyFoxCoordinator = hass.data[DOMAIN_MYFOX][entry.entry_id]
+    coordinator:MyFoxCoordinator = hass.data.setdefault(MYFOX_KEY, {})[entry.entry_id]
     for (client_key,client_item) in coordinator.myfoxApiClients.items() :
         client: MyFoxApiClient = client_item
 
