c2218e5aa558e10456d43f35ef598c9e4457874a
Angelius007 <github@angelius.bzh>
2024-11-24 12:29:24 +0100

    update nb max retry per client

--- custom_components/myfox/api/myfoxapi.py
+++ custom_components/myfox/api/myfoxapi.py
@@ -59,6 +59,7 @@ class MyFoxApiClient:
     def __init__(self, myfox_info:MyFoxEntryDataApi) -> None:
         self.client_key = "generic"
         self.client = None
+        self.nb_retry = 5
         self.devices: dict[str, BaseDevice] = {}
         self.scenes: dict[str, BaseScene] = {}
         self.infoSites_times = 0
@@ -187,18 +188,18 @@ class MyFoxApiClient:
                         resp = await session.get(urlApi, headers=headers, json=data) 
                         retour = await self._get_response(resp, responseClass)
                 if retry > 0 :
-                    _LOGGER.info(f"Relance de la requete {path} : OK (Tentative : {(retry)}/5)")
+                    _LOGGER.info(f"Relance de la requete {path} : OK (Tentative : {(retry)}/{self.nb_retry})")
                 return retour
             except InvalidTokenMyFoxException as exception:
                 raise exception
             except MyFoxException as exception:
                 """ Retry """
-                if retry < 5 :
-                    _LOGGER.warning(f"Erreur {exception.status}. Relance de la requete {path} (Tentative : {(retry+1)}/5)")
+                if retry < self.nb_retry :
+                    _LOGGER.warning(f"Erreur {exception.status}. Relance de la requete {path} (Tentative : {(retry+1)}/{self.nb_retry})")
                     await asyncio.sleep(20) # tempo de qqes secondes pour relancer la requete
                     return await self.callMyFoxApi(path, data, method, responseClass, (retry+1))
                 else :
-                    _LOGGER.error(f"Erreur {exception.status}. Echec des relances {path} (Tentative : {(retry)}/5)")
+                    _LOGGER.error(f"Erreur {exception.status}. Echec des relances {path} (Tentative : {(retry)}/{self.nb_retry})")
                     raise exception
             except Exception as exception:
                 _LOGGER.error(exception)
