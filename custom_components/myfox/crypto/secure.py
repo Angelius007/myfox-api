d5d15fcd500bb6a16304af363aee74c8c845846b
Angelius007 <github@angelius.bzh>
2025-02-09 16:56:34 +0100

    fix encoding

--- custom_components/myfox/crypto/secure.py
+++ custom_components/myfox/crypto/secure.py
@@ -7,13 +7,12 @@ CODE_SEPARATOR: str = "&&"
 
 def encode(string:str, password:str):
     """
-    Base64 encoding schemes are commonly used when there is a need to encode 
-    binary data that needs be stored and transferred over media that are 
-    designed to deal with textual data.
-    This is to ensure that the data remains intact without 
-    modification during transport.
+    encode code
     """
-    return base64.b64encode((password + CODE_SEPARATOR + string).encode(ENCODE_BYTES)).decode(ENCODE_BYTES)
+    return base64.b64encode((str(password) + CODE_SEPARATOR + str(string)).encode(ENCODE_BYTES)).decode(ENCODE_BYTES)
 
 def decode(string:str, password:str):
-    return base64.b64decode(string.encode(ENCODE_BYTES)).decode(ENCODE_BYTES).replace(password + CODE_SEPARATOR, "", 1)
+    """
+    decode code
+    """
+    return base64.b64decode(string.encode(ENCODE_BYTES)).decode(ENCODE_BYTES).replace(str(password) + CODE_SEPARATOR, "", 1)
